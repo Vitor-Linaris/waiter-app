73a63bc5378745e575b8e993e97ab05942fdaf3d
Vitor-Linaris <vitor.linaris@gmail.com>
2025-08-31 06:11:50 -0300

    cors: add mongoose and config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
   },
   {
     languageOptions: { globals: globals.node },
   },
   {
     rules: {
-      quotes: ["error", "single", { avoidEscape: true }],
+      quotes: ['error', 'single', { avoidEscape: true }],
       // "single" = usa aspas simples
       // avoidEscape = deixa usar aspas duplas quando necessário (ex: para strings com apóstrofo)
     },
