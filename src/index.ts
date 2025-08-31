6acde2d660662b10a292dad118b476287c9cc118
Vitor-Linaris <vitor.linaris@gmail.com>
2025-08-31 03:47:43 -0300

    Init project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    const app = express();
+
+    const PORT = 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Failed to connect to MongoDB");
+  });
